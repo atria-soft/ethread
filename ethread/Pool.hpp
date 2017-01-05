5322b71c00280b0a7c6445751d0464015b2250ed
Edouard DUPIN <yui.heero@gmail.com>
2017-01-05 21:28:23 +0100

    Change licence APACHE-2 to MPL-2 ==> force source redistribution and permit static link

--- ethread/Pool.hpp
+++ ethread/Pool.hpp
@@ -1,7 +1,7 @@
 /**
  * @author Edouard DUPIN
  * @copyright 2011, Edouard DUPIN, all right reserved
- * @license APACHE v2.0 (see license file)
+ * @license MPL v2.0 (see license file)
  */
 #pragma once
 
--- lutin_ethread-tools.py
+++ lutin_ethread-tools.py
@@ -10,7 +10,7 @@ def get_desc():
 	return "Ewol thread tools"
 
 def get_licence():
-	return "APACHE-2"
+	return "MPL-2"
 
 def get_compagny_type():
 	return "com"
